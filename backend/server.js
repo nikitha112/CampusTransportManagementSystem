01c9df9f76e11974cf0a37952c4fa022725e0d9a
nikitha112 <nikithaburle@gmail.com>
2025-11-08 12:08:04 +0530

    Added root route for Render test

--- backend/server.js
+++ backend/server.js
@@ -842,6 +842,9 @@ app.get("/api/routes/report/:routeName", authenticateToken, authorizeRoles("admi
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
 
 
 
@@ -850,6 +853,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
 
 
